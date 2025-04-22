f97e149c14c41d3873b4b22c4efd1e3dde9c0238
mcas0215 <kang33089343@gmail.com>
2025-04-22 17:12:54 +0900

    feat shop

--- Sparta_TextRPG/Sparta_TextRPG/Animal/Shop.cs
+++ Sparta_TextRPG/Sparta_TextRPG/Animal/Shop.cs
@@ -15,6 +15,7 @@ namespace Sparta_TextRPG
         //왜 item-void 이고 string-item인지
         //함수를 SellItem, AddItem, 해야하는데 buyitem에선 사람이 구매를 누르고 장비에 따라 번호를 누르면 그 번호에 해당하는 게 얼마인지에 따라 본인 돈을 확인 뒤 돈이적은지 많은지에 따라 사지거나 못 산다는 문구를 표출.
         //구매를 하면 장비는 인벤토리에도 들어가야 하고, 상점에는 구매 완료 라고 떠야 함.
+        //그럼 함수만 넣어야한다면 ButItem에는 사람이 번호를 누르고
         //sellitem 에서는 사람이 판매를 누르고 장비에 따라 번호를 누르면 그 장비가 팔리게 됨
         //판매를 하면 장비는 인벤토리에서 없어져야 하고상점에는 다시 값어치로 뜨게 됨
 
@@ -35,6 +36,7 @@ namespace Sparta_TextRPG
 
         public void BuyItem(Item item)
         {
+
             Console.WriteLine("구매 가능한 아이템 목록");
             Inventory.Add(item);
             for (int i = 0; i < ShopItems.Count; i++);
@@ -51,11 +53,20 @@ namespace Sparta_TextRPG
             if (int.TryParse(input, out int choice) && choice > 0 && choice <= shopItems.Count)
             { }
                   
+
+            // item 을 받고 
+            // 유무 확인
+            // Inventory.Add(item);
+
+            
+            
+
         }
 
 
 
 
+        
 
 
 
@@ -63,8 +74,29 @@ namespace Sparta_TextRPG
 
 
 
-        public void SellItem()
-        {
+
+        
+     public void SellItem(Player player, int selectedIndex)
+
+        { 
+            List<Item> shopItems = Inventory.GetAllItems();
+
+
+        if (selectedIndex < 0 || selectedIndex >= shopItems.Count)
+                return;
+
+            Item selectedItem = shopItems[selectedIndex];
+
+            if (player.Gold < selectedItem.Price)
+                return;
+
+
+            player.Gold -= selectedItem.Price;
+
+
+            player.Inventory.Add(selectedItem); 
+        
+        
 
         }
     }
