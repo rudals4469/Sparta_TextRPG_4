8c1411f901639f555eeeed5da34f44b7e0de16a6
mcas0215 <kang33089343@gmail.com>
2025-04-22 11:50:37 +0900

    feat shop

--- Sparta_TextRPG/Sparta_TextRPG/Animal/Shop.cs
+++ Sparta_TextRPG/Sparta_TextRPG/Animal/Shop.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +10,34 @@ namespace Sparta_TextRPG
     internal class Shop
     {
         public Inventory Inventory { get; set; }
+
+        //왜 item-void 이고 string-item인지
+        //함수를 SellItem, AddItem, 
+
+        public void show() 
+        {
+            Console.WriteLine("상점에 오신 것을 환영합니다");   
+        
+        }
+        public void main()
+        {
+            show();
+        }
+
+        public void AddItem(Item item)
+        {
+            Inventory.Add(item);
+        }
+
+        public void BuyItem()
+        {
+
+        }
+        
+
+        public void SellItem()
+        {
+
+        }
     }
 }
